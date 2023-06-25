c91c7597ad53bc8eecaa48272438fcced9814ec3
Donata0219 <Donata.Radionovo@zarasupm.lt>
2023-06-25 21:36:37 +0300

    prideti moduliai elektrosskaitliukas ir saskaitos

--- namo_bendrija/buhalterija/admin.py
+++ namo_bendrija/buhalterija/admin.py
@@ -2,12 +2,32 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Skaitiklis, Savininkas, Butas
+from .models import Skaitiklis, Savininkas, Butas, Saskaita, Elektrosskaitiklis
+
 
 class SkaitiklisAdmin(admin.ModelAdmin):
     readonly_fields = ("skirtumas",)
     list_display = ("skaitiklio_vieta", "nuo_reiksme", "iki_reiksme",)
 
+class SaskaitaAdmin(admin.ModelAdmin):
+    list_display = [
+        "gyvatukas",
+        "bendra_elektra",
+        "karsto_vandens_ikainis",
+        "salto_vandens_ikainis",
+        "kaupiamasis",
+        "administravimo",
+    ]
+
+class ElektrosskaitiklisAdmin(admin.ModelAdmin):
+    readonly_fields = ("skirtumas_el", "buto_el")
+    list_display = ("nuo_reiksme_el", "iki_reiksme_el", "ikainis_el",)
+
+class ButasAdmin (admin.ModelAdmin):
+    list_display = ("buto_numeris", "buto_plotas", "zmoniu_skaicius")
+
 admin.site.register(Skaitiklis, SkaitiklisAdmin)
 admin.site.register(Savininkas)
-admin.site.register(Butas)
+admin.site.register(Butas, ButasAdmin)
+admin.site.register(Saskaita, SaskaitaAdmin)
+admin.site.register(Elektrosskaitiklis, ElektrosskaitiklisAdmin)
--- namo_bendrija/buhalterija/models.py
+++ namo_bendrija/buhalterija/models.py
@@ -3,6 +3,9 @@ import datetime
 from django.db import models
 
 # Create your models here.
+
+
+
 class Skaitiklis (models.Model):
 
     PIRMAS = "Šaltas vonios"
@@ -58,5 +61,42 @@ class Butas (models.Model):
         return f"{self.buto_numeris}  {self.savininkas} {self.buto_plotas} {self.zmoniu_skaicius}"
 
 
+class Elektrosskaitiklis(models.Model):
+    nuo_reiksme_el = models.IntegerField(verbose_name="Nuo", null=True, blank=True)
+    iki_reiksme_el = models.IntegerField(verbose_name="Iki")  # įvesti skaitilnių parodymus
+    created_at = models.DateTimeField(auto_now_add=True)
+    skirtumas_el = models.IntegerField(verbose_name="Skirtumas")
+    ikainis_el = models.FloatField()
+    buto_el = models.FloatField() #mokama suma uz bendra elektros suvartojima, padalinta visiems butams
+    # butas = models.ForeignKey("Butas", on_delete=models.SET_NULL, null=True)
 
+    def save(self, *args, **kwargs):
+        try:
+            buves_irasas_el = Elektrosskaitiklis.objects.last()
+            buvusi_iki_reiksme_el = buves_irasas_el.iki_reiksme_el
+            self.nuo_reiksme_el = buvusi_iki_reiksme_el
+        except AttributeError:
+            pass
+
+        self.skirtumas_el = self.iki_reiksme_el - self.nuo_reiksme_el
+        self.buto_el = self.skirtumas_el * self.ikainis_el / 35
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.buto_el}"
+
+
+class Saskaita (models.Model):
+    # butas = models.ForeignKey(Butas, on_delete=models.SET_NULL, null=True )
+    # skaitiklis = models.ForeignKey(Skaitiklis, on_delete=models.SET_NULL, null=True)# ar turi buti SET_NULL, ar CASCADE?
+    gyvatukas = models.FloatField(verbose_name="Gyvatukas")
+    bendra_elektra = models.FloatField(verbose_name="Bendra elektra")
+    karsto_vandens_ikainis = models.FloatField(verbose_name="Karšto vandens įkainis")
+    salto_vandens_ikainis = models.FloatField(verbose_name="Šalto vandens įkainis")
+    kaupiamasis = models.IntegerField(verbose_name="Kaupiamieji remontui")
+    administravimo = models.IntegerField(verbose_name="Administravimo mokestis")
+
+    def __str__(self):
+        return f"{self.gyvatukas} {self.bendra_elektra} {self. karsto_vandens_ikainis} {self.salto_vandens_ikainis} {self.kaupiamasis} {self.administravimo}"                f""
 
